626a8af07a447c59043aa7b49151a3809b2a0748
externref <shiva02939@gmail.com>
2024-09-02 19:32:16 +0530

    setup for gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,10 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		
+	},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
@@ -16,7 +20,7 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: "404.html",
 			precompress: false,
 			strict: true
 		})
