f6eba86fa6ed441c3537de6f490864c37618dfe0
externref <shiva02939@gmail.com>
2024-09-02 19:34:59 +0530

    ...

--- svelte.config.js
+++ svelte.config.js
@@ -7,23 +7,13 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	kit: {
+		kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		
-	},
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: "404.html",
-			precompress: false,
-			strict: true
-		})
+		}
 	}
 };
 
